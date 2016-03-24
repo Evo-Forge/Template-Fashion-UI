0bedb14d6c115cb054c947f9f0a99385464c2dc1
Radu Farcas <f.radu10@yahoo.com>
2016-03-24 11:43:53 +0200

    first commit

--- js/components/commerce.js
+++ js/components/commerce.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ClassNames from 'classnames';
 
+import { Block, Btn, Card, CardHeader, CardContent, CardFooter} from 'react-essence';
+
+
 class PatagoniaCommerce extends React.Component {
 
   render() {
     return (
-      <Block>
+      <Block classes={'e-background-indigo-600'}>
+        <Block classes={'patagoniaCommerce e-container'}>
+          <Card>
+            <CardHeader>
+             Card header
+            </CardHeader>
+            <CardContent>
+             Card content
+            </CardContent>
+            <CardFooter>
+             Card footer with action buttons
+            </CardFooter>
+          </Card>
+        </Block>
       </Block>
     );
   }
