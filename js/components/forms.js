3773dcfe8992feafd46c526891ecf390f415d36e
Oana Peres <oana.peres@evozon.com>
2016-03-24 02:44:38 -0700

    block review

--- js/components/forms.js
+++ js/components/forms.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ClassNames from 'classnames';
 
+import {Block, Text} from 'essence-core';
+
 class PatagoniaForms extends React.Component {
 
   render() {
     return (
-      <Block>
+      <Block className={'e-background-gray-200'}>
+        <Block className={'patagoniaForms e-container'}>
+          <Text type={'h1'} classes={'e-text-center e-body1'}>
+          <Text type={'span'} classes={'e-text-center e-body1 e-text-blue-900'}>4</Text>
+          FORMS</Text>
+        </Block>
       </Block>
     );
   }
