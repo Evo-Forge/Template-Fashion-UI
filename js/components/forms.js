26c1b3f01073c25a62840eef9bf19cf6ef07d9e8
Oana Peres <oana.peres@evozon.com>
2016-03-28 08:06:19 -0700

    add account settings and stepper

--- js/components/forms.js
+++ js/components/forms.js
@@ -11,7 +11,70 @@ import {Block,
   List, ListItem, Switch,
   Text } from 'react-essence';
 
+import Stepper from 'essence-stepper';
 
+const Steps = {
+ 'editable': [
+  {
+   'title': (<Text>Cart</Text>),
+   'content': (
+     <Block classes={'e-background-white'}>
+       <List type={'navigation'} classes={'e-twolinelist'}>
+         <ListItem>
+            <Text type={'a'}>
+              <Image src={'assets/img/blue-tshirt.jpg'} alt={'TShirt'} height={'75'} classes={'e-left'}/>
+              <Block classes={'content e-left btn-margin-nr'}>
+                <Text classes={'primary e-text-left'}>Blue TShirt</Text>
+                <Text classes={'secondary e-text-left'}>Hooligan</Text>
+              </Block>
+              <Block className={'e-left border-btn-nr margin-nr'}>
+                <Btn label={'-'} ripple={false} type={'default'} className={'btn-margin-nr e-background-white'} />
+                <Btn label={'3'} ripple={false} type={'default'} className={'btn-margin-nr e-background-white'} />
+                <Btn label={'+'} ripple={false} type={'default'} className={'btn-margin-nr e-background-white'} />
+              </Block>
+              <Btn label={'M'} ripple={false} type={'default'} className={'e-left e-background-grey-200 btn-no-border'} />
+              <Text type={'span'} classes={'e-text-center e-left e-button'}>$250</Text>
+              <Icon name={"navigation-close"} className={"e-text-cyan-A200"} />
+            </Text>
+         </ListItem>
+         <ListItem>
+           <Text type={'a'}>
+             <Image src={'http://i.imgur.com/xrDnt12.png'} alt={'Star Wars'} classes={'e-avatar e-left'}/>
+             <Block classes={'content e-left'}>
+               <Text classes={'primary'}>Uncle Bear</Text>
+               <Text classes={'secondary'}>Jan 10, 2016</Text>
+             </Block>
+             <Icon name={'action-info'} classes={'e-right e-text-grey-500'} />
+           </Text>
+         </ListItem>
+         <ListItem>
+           <Text type={'a'}>
+             <Image src={'http://i.imgur.com/DhsdGIs.jpg'} alt={'Star Wars'} classes={'e-avatar e-left'}/>
+             <Block classes={'content e-left'}>
+               <Text classes={'primary'}>Mutant Ninja</Text>
+               <Text classes={'secondary'}>Jan 11, 2016</Text>
+             </Block>
+             <Icon name={'action-info'} classes={'e-right e-text-grey-500'} />
+           </Text>
+         </ListItem>
+       </List>
+     </Block>
+   )
+  },
+  {
+   'title': (<Text>Checkout</Text>),
+   'content': (<Text className={'e-caption'}>Ad group</Text>)
+  },
+  {
+   'title': (<Text>Shipping</Text>),
+   'content': (<Text className={'e-caption'}>Ad details</Text>)
+ },
+ {
+  'title': (<Text>Done</Text>),
+  'content': (<Text className={'e-caption'}>Ad details</Text>)
+ }
+ ]
+ };
 
 class PatagoniaForms extends React.Component {
 
@@ -232,27 +295,110 @@ class PatagoniaForms extends React.Component {
                     <Text type={'p'} classes={'e-text-left e-text-white e-body1 e-margin-top-15'}>+(04) 745.123.525</Text>
                   </Block>
 
-                  <Block classes={'e-right half-width padding-card'}>
-                    <Block classes={'full-width border-header-card'}>
+                  <Block classes={'e-right half-width e-background-white'}>
+                    <Block classes={'full-width border-header-card padding-card-contact'}>
                       <Text type={'p'} classes={'e-text-left e-body1'}>Contact Us</Text>
                     </Block>
-                    <Input type={'text'} name={'label'} label={'Your Name'} classes={'e-text-grey-300 input-look'}/>
-                    <Input type={'email'} name={'label'} label={'Your Email'} classes={'e-text-grey-300 input-look'}/>
-                    <Input type={'textarea'} name={'label'} rows={5} label={'Your Message'} classes={'input-look'}/>
-                    <Block classes={'e-left mg-top-attach'}>
-                      <Text type={'a'}>
-                        <Icon name={"editor-attach-file"} className={"e-text-cyan-A200 e-left"} />
-                        <Block classes={'content e-left e-no-padding'}>
-                          <Text type={'span'} classes={'e-text-left e-text-cyan-A200'}>Attach file</Text>
-                        </Block>
-                      </Text>
+                    <Block classes={'padding-card-contact'}>
+                      <Input type={'text'} name={'label'} label={'Your Name'} classes={'e-text-grey-300 input-look'}/>
+                      <Input type={'email'} name={'label'} label={'Your Email'} classes={'e-text-grey-300 input-look'}/>
+                      <Input type={'textarea'} name={'label'} rows={5} label={'Your Message'} classes={'input-look'}/>
+                      <Block classes={'e-left mg-top-attach '}>
+                        <Text type={'a'}>
+                          <Icon name={"editor-attach-file"} className={"e-text-cyan-A200 e-left"} />
+                          <Block classes={'content e-left e-no-padding'}>
+                            <Text type={'span'} classes={'e-text-left e-text-cyan-A200'}>Attach file</Text>
+                          </Block>
+                        </Text>
+                      </Block>
+                      <Block className={'e-right'}>
+                        <Btn label={'Sign in'} ripple={true} type={'submit'} className={'flat e-margin-top-15 e-margin-bottom-15 e-button e-text-white e-background-cyan-A200 btn-look'} />
+                      </Block>
                     </Block>
-                    <Block className={'e-right'}>
-                      <Btn label={'Sign in'} ripple={true} type={'submit'} className={'flat e-margin-top-15 e-margin-bottom-15 e-button e-text-white e-background-cyan-A200 btn-look'} />
-                    </Block>
-                    </Block>
+                  </Block>
                 </CardContent>
               </Card>
+            </Block>
+          </Block>
+
+          <Block classes={'e-row'}>
+            <Block classes={'brick brick-6 e-h-end e-v-end'}>
+              <Card classes={'full-width e-background-white no-padding-card card-h-account'}>
+                <CardHeader classes={'e-text-left border-header-card'}>
+                  <Text type={'span'} classes={'e-text-left e-body1'}>Account Settings</Text>
+                  <Icon name={"navigation-close"} className={"e-text-black e-right"} />
+                </CardHeader>
+               <CardContent>
+                <Block className={'card-content-header'}>
+                  <Image src={'assets/img/poza-profil.jpg'} alt={'John Smith'} classes={'e-left avatar-card'}/>
+                  <Icon name={"navigation-expand-more"} className={'e-left e-text-white e-background-cyan-A200 round-icon'} />
+                  <Block classes={'e-left padding-left-para'}>
+                    <Text type={'h2'} classes={'e-text-left e-headline'}>John Smith</Text>
+                    <Text type={'p'} classes={'e-text-center e-body1 e-text-grey-600'}>Senior Product Designer (UI/UX)</Text>
+                  </Block>
+                </Block>
+                <Block className={'card-content-account e-text-center'}>
+                  <Input type={'text'} name={'label'} label={'First Name'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                  <Input type={'text'} name={'label'} label={'Last Name'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                  <Input type={'text'} name={'label'} label={'USA'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                  <Input type={'text'} name={'label'} label={'Position'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                  <Input type={'text'} name={'label'} label={'Address'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                  <Input type={'text'} name={'label'} label={'Phone'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                  <Input type={'email'} name={'label'} label={'Email'} classes={'e-text-grey-300 input-look full-width'}/>
+                </Block>
+                <Block className={'e-text-center btn-inter'}>
+                  <Btn label={'Save'} ripple={true} type={'submit'} className={'flat e-margin-top-15 e-margin-bottom-25 e-button e-text-white e-background-cyan-A200 btn-save-w'} />
+                </Block>
+               </CardContent>
+              </Card>
+            </Block>
+
+            <Block classes={'brick brick-6 e-h-start e-v-end'}>
+              <Block classes={'e-row'}>
+                <Block classes={'brick brick-12 e-h-center e-v-center'}>
+                  <Card classes={'e-background-white no-padding-card'}>
+                    <CardHeader classes={'e-text-left border-header-card'}>
+                      <Text type={'span'} classes={'e-text-left e-body1'}>Credit Card Detail</Text>
+                      <Icon name={"navigation-close"} className={"e-text-black e-right"} />
+                    </CardHeader>
+                    <CardContent>
+                      <Block className={'card-content-account'}>
+                        <Input type={'text'} name={'label'} label={'1234 1234 1234 1234'} classes={'e-text-grey-300 input-look input-focus full-width'}/>
+                        <Input type={'text'} name={'label'} label={'Month'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                        <Input type={'text'} name={'label'} label={'Year'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                        <Input type={'text'} name={'label'} label={'CVV'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                        <Block classes={'half-w-input padding-t-para'}>
+                          <Text type={'p'} classes={'e-text-left e-body1'}>3 or 4 digits usually found</Text>
+                          <Text type={'p'} classes={'e-text-left e-body1'}>on the signature strip</Text>
+                        </Block>
+                      </Block>
+                      <Block className={'e-text-center btn-inter'}>
+                        <Btn label={'Save'} ripple={true} type={'submit'} className={'raised e-margin-top-15 e-margin-bottom-25 e-button e-text-white e-background-indigo-400 btn-save-w'} />
+                      </Block>
+                    </CardContent>
+                  </Card>
+                </Block>
+
+                <Block classes={'brick brick-12 e-h-center e-v-center'}>
+                  <Card classes={'e-background-indigo-400 full-width'}>
+                    <CardHeader classes={'e-background-indigo-500 e-text-center'}>
+                      <Text type={'span'} classes={'e-text-white e-body1'}>Subscribe to our newsletter </Text>
+                    </CardHeader>
+                    <CardContent className={'display-card-subscribe'}>
+                      <Input type={'text'} name={'label'} label={'home@gmail.com'} classes={'e-text-grey-300 input-look half-w-input'}/>
+                      <Btn label={'Submit'} ripple={true} type={'submit'} className={'flat e-button e-text-white e-background-cyan-A200 half-w-input submit-h'} />
+                    </CardContent>
+                  </Card>
+                </Block>
+              </Block>
+            </Block>
+          </Block>
+
+          <Block classes={'e-row'}>
+            <Block classes={'brick brick-12 e-h-center e-v-center'}>
+              <Block className={'full-width'}>
+                <Stepper steps={Steps['editable']} classes={'cart'}/>
+              </Block>
             </Block>
           </Block>
         </Block>
