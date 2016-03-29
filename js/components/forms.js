99e6c6c94bc9cb9711b9fec42835d9850472a8e7
Oana Peres <oana.peres@evozon.com>
2016-03-29 08:01:45 -0700

    add stepper

--- js/components/forms.js
+++ js/components/forms.js
@@ -18,11 +18,11 @@ const Steps = {
   {
    'title': (<Text>Cart</Text>),
    'content': (
-     <Block classes={'e-background-white'}>
-       <List type={'navigation'} classes={'e-twolinelist'}>
-         <ListItem>
+      <Block classes={'e-background-white'}>
+        <List type={'navigation'} classes={'e-twolinelist e-no-padding'}>
+          <ListItem classes={'padding-list-stepper'}>
             <Text type={'a'}>
-              <Image src={'assets/img/blue-tshirt.jpg'} alt={'TShirt'} height={'75'} classes={'e-left'}/>
+              <Image src={'assets/img/blue-tshirt.jpg'} alt={'TShirt'} classes={'article-image e-left'}/>
               <Block classes={'content e-left btn-margin-nr'}>
                 <Text classes={'primary e-text-left'}>Blue TShirt</Text>
                 <Text classes={'secondary e-text-left'}>Hooligan</Text>
@@ -32,46 +32,142 @@ const Steps = {
                 <Btn label={'3'} ripple={false} type={'default'} className={'btn-margin-nr e-background-white'} />
                 <Btn label={'+'} ripple={false} type={'default'} className={'btn-margin-nr e-background-white'} />
               </Block>
-              <Btn label={'M'} ripple={false} type={'default'} className={'e-left e-background-grey-200 btn-no-border'} />
-              <Text type={'span'} classes={'e-text-center e-left e-button'}>$250</Text>
+              <Btn label={'M'} ripple={false} type={'default'} className={'e-left e-background-grey-200 btn-no-border margin-nr'} />
+              <Text type={'span'} classes={'e-text-center e-left e-text-uppercase margin-nr'}>$250</Text>
               <Icon name={"navigation-close"} className={"e-text-cyan-A200"} />
             </Text>
-         </ListItem>
-         <ListItem>
-           <Text type={'a'}>
-             <Image src={'http://i.imgur.com/xrDnt12.png'} alt={'Star Wars'} classes={'e-avatar e-left'}/>
-             <Block classes={'content e-left'}>
-               <Text classes={'primary'}>Uncle Bear</Text>
-               <Text classes={'secondary'}>Jan 10, 2016</Text>
-             </Block>
-             <Icon name={'action-info'} classes={'e-right e-text-grey-500'} />
-           </Text>
-         </ListItem>
-         <ListItem>
-           <Text type={'a'}>
-             <Image src={'http://i.imgur.com/DhsdGIs.jpg'} alt={'Star Wars'} classes={'e-avatar e-left'}/>
-             <Block classes={'content e-left'}>
-               <Text classes={'primary'}>Mutant Ninja</Text>
-               <Text classes={'secondary'}>Jan 11, 2016</Text>
-             </Block>
-             <Icon name={'action-info'} classes={'e-right e-text-grey-500'} />
-           </Text>
-         </ListItem>
-       </List>
-     </Block>
+          </ListItem>
+          <ListItem classes={'padding-list-stepper e-background-grey-100'}>
+            <Text type={'a'}>
+              <Image src={'assets/img/poza-pantalon.jpg'} alt={'trausers'} classes={'article-image e-left'}/>
+              <Block classes={'content e-left btn-margin-nr'}>
+                <Text classes={'primary e-text-left'}>Navy Trousers</Text>
+                <Text classes={'secondary e-text-left e-text-uppercase'}>Navy god</Text>
+              </Block>
+              <Block className={'e-left border-btn-nr margin-nr e-background-grey-400'}>
+                <Btn label={'-'} ripple={false} type={'default'} className={'btn-margin-nr e-text-white e-background-grey-400'} />
+                <Btn label={'6'} ripple={false} type={'default'} className={'btn-margin-nr e-text-white e-background-grey-400'} />
+                <Btn label={'+'} ripple={false} type={'default'} className={'btn-margin-nr e-text-white e-background-grey-400'} />
+              </Block>
+              <Btn label={'XS'} ripple={false} type={'default'} className={'e-left e-background-white btn-no-border margin-nr'} />
+              <Text type={'span'} classes={'e-text-center e-left e-text-uppercase margin-nr'}>$145</Text>
+              <Icon name={"navigation-close"} className={"e-text-cyan-A200"} />
+            </Text>
+          </ListItem>
+          <ListItem classes={'padding-list-stepper'}>
+            <Text type={'a'}>
+              <Image src={'assets/img/watch.jpg'} alt={'TShirt'} classes={'article-image e-left'}/>
+              <Block classes={'content e-left btn-margin-nr'}>
+                <Text classes={'primary e-text-left'}>Peaceful Hooligan</Text>
+                <Text classes={'secondary e-text-left e-text-uppercase'}>Sold by Kgr</Text>
+              </Block>
+              <Block className={'e-left border-btn-nr margin-nr'}>
+                <Btn label={'-'} ripple={false} type={'default'} className={'btn-margin-nr e-background-white'} />
+                <Btn label={'3'} ripple={false} type={'default'} className={'btn-margin-nr e-background-white'} />
+                <Btn label={'+'} ripple={false} type={'default'} className={'btn-margin-nr e-background-white'} />
+              </Block>
+              <Btn label={'XL'} ripple={false} type={'default'} className={'e-left e-background-grey-200 btn-no-border margin-nr'} />
+              <Text type={'span'} classes={'e-text-center e-left e-text-uppercase margin-nr'}>$370</Text>
+              <Icon name={"navigation-close"} className={"e-text-cyan-A200"} />
+            </Text>
+          </ListItem>
+          <ListItem classes={'padding-list-stepper border-t-discount e-padding-top-15 e-padding-15'}>
+            <Text type={'a'}>
+              <Text type={'span'} classes={'e-button e-left align-discount'}>Discount: <b> -120$</b></Text>
+              <Text type={'p'} classes={'e-text-grey-400 e-text-right e-body1 e-text-uppercase'}>Total:
+              <Text type={'span'} classes={'e-text-right e-text-uppercase e-text-black e-subhead padding-r-para'}> &645</Text></Text>
+            </Text>
+          </ListItem>
+          <ListItem classes={'padding-list-stepper e-background-grey-100 e-padding-top-15 e-padding-bottom-15'}>
+            <Text type={'a'}>
+              <Btn label={'Back to shop'} ripple={false} type={'default'} className={'flat e-button e-left e-background-grey-300'} />
+              <Btn label={'Proceed to checkout'} ripple={true} type={'default'} classes={'flat e-background-indigo-400 e-text-white align-btn-continue'} />
+            </Text>
+          </ListItem>
+        </List>
+      </Block>
    )
   },
   {
    'title': (<Text>Checkout</Text>),
-   'content': (<Text className={'e-caption'}>Ad group</Text>)
+   'content': (
+     <Block className={'e-background-white'}>
+      <Block className={'checkout-w'}>
+        <Block className={'card-content-account'}>
+          <Input type={'text'} name={'label'} label={'1234 1234 1234 1234'} classes={'e-text-grey-300 input-look input-focus full-width'}/>
+          <Input type={'text'} name={'label'} label={'Month'} classes={'e-text-grey-300 input-look half-w-input'}/>
+          <Input type={'text'} name={'label'} label={'Year'} classes={'e-text-grey-300 input-look half-w-input'}/>
+          <Input type={'text'} name={'label'} label={'CVV'} classes={'e-text-grey-300 input-look half-w-input'}/>
+          <Block classes={'half-w-input padding-t-para'}>
+            <Text type={'p'} classes={'e-text-left e-caption e-no-margin e-text-black'}>3 or 4 digits usually found</Text>
+            <Text type={'p'} classes={'e-text-left e-caption e-no-margin e-text-black'}>on the signature strip</Text>
+          </Block>
+        </Block>
+      </Block>
+      <Block>
+        <Text type={'a'} classes={'e-text-center e-body1 e-text-cyan-A200 e-margin-top-15 a-inter'}>Or pay with PayPal</Text>
+      </Block>
+        <Block className={'checkout-w'}>
+        <Block className={'card-content-account'}>
+          <Input type={'email'} name={'label'} label={'Email Address'} classes={'e-text-grey-300 input-look half-w-input'}/>
+          <Block classes={'half-w-input'}>
+            <Btn label={'PayPal'} ripple={true} type={'default'} classes={'flat e-background-indigo-900 e-text-white full-width margin-t-paypal'} />
+          </Block>
+        </Block>
+      </Block>
+      <Block classes={'e-background-grey-100 e-padding-top-15'}>
+        <Block className={'footer-checkout-w'}>
+          <Btn label={'Back to shop'} ripple={false} type={'default'} className={'flat e-button e-left e-background-grey-300 e-margin-bottom-15'} />
+          <Btn label={'Pay $645'} ripple={true} type={'default'} classes={'flat e-background-indigo-400 e-text-white align-btn-continue e-right btn-look'} />
+        </Block>
+      </Block>
+    </Block>
+   )
   },
   {
    'title': (<Text>Shipping</Text>),
-   'content': (<Text className={'e-caption'}>Ad details</Text>)
+   'content': (
+      <Block className={'e-background-white'}>
+        <Block className={'checkout-w'}>
+          <Block className={'card-content-account e-text-center'}>
+            <Input type={'text'} name={'label'} label={'First Name'} classes={'e-text-grey-300 input-look half-w-input'}/>
+            <Input type={'text'} name={'label'} label={'Last Name'} classes={'e-text-grey-300 input-look half-w-input'}/>
+            <Input type={'email'} name={'label'} label={'Email'} classes={'e-text-grey-300 input-look half-w-input'}/>
+            <Input type={'text'} name={'label'} label={'Phone'} classes={'e-text-grey-300 input-look half-w-input'}/>
+            <Input type={'text'} name={'label'} label={'Country'} classes={'e-text-grey-300 input-look third-w-input'}/>
+            <Input type={'text'} name={'label'} label={'City'} classes={'e-text-grey-300 input-look third-w-inputt'}/>
+            <Input type={'email'} name={'label'} label={'Zip Code'} classes={'e-text-grey-300 input-look third-w-input'}/>
+            <Input type={'text'} name={'label'} label={'State'} classes={'e-text-grey-300 input-look third-w-input'}/>
+            <Input type={'text'} name={'label'} label={'Address'} classes={'e-text-grey-300 input-look two-third-w-input'}/>
+          </Block>
+        </Block>
+        <Block classes={'e-background-grey-100 e-padding-top-15'}>
+          <Block className={'footer-checkout-w'}>
+            <Btn label={'Back to shop'} ripple={false} type={'default'} className={'flat e-button e-left e-background-grey-300 e-margin-bottom-15'} />
+            <Btn label={'Pay $645'} ripple={true} type={'default'} classes={'flat e-background-indigo-400 e-text-white align-btn-continue e-right btn-look'} />
+          </Block>
+        </Block>
+     </Block>
+   )
  },
  {
   'title': (<Text>Done</Text>),
-  'content': (<Text className={'e-caption'}>Ad details</Text>)
+  'content': (
+    <Block className={'e-background-white'}>
+      <Block className={'checkout-w e-padding-top-50 e-padding-bottom-25'}>
+        <Block className={'bl-done'}>
+          <Icon name={"action-done"} className={"e-background-cyan-A200 e-text-white e-display-2 done-icon"} />
+        </Block>
+        <Text type={'p'} classes={'e-text-center e-text-grey-800 e-display-2 e-padding-top-25'}>Congratulations! Your Order is Complete!</Text>
+        <Text type={'p'} classes={'e-text-center e-text-grey-800 padding-list-stepper'}>
+          Lorem ipsum dolor sit amet, sed eu prima consectetuer:<br/> Duis habeo mediocrem an sed, qui id utamur complectitur, te ignota constituam vituperatoribus est. No errem luptatum pri.
+        </Text>
+      </Block>
+      <Block classes={'e-background-grey-100 e-padding-top-15 e-padding-bottom-15'}>
+          <Btn label={'Back to Shop'} ripple={true} type={'default'} classes={'flat e-background-indigo-400 e-text-white btn-look'} />
+      </Block>
+    </Block>
+  )
  }
  ]
  };
@@ -81,8 +177,9 @@ class PatagoniaForms extends React.Component {
   render() {
     return (
       <Block className={'e-background-grey-100'}>
-        <Block className={'patagoniaForms e-container'}>
-          <Text type={'h1'} classes={'e-text-center e-display-2 e-padding-top-50 background-rainbow'}> FORMS </Text>
+        <Block className={'patagoniaForms e-container e-padding-bottom-150'}>
+
+          <Text type={'h1'} classes={'e-text-center e-display-2 e-padding-top-50 e-padding-bottom-50 background-rainbow e-no-margin'}> FORMS </Text>
           <Block classes={'e-row'}>
             <Block classes={'brick brick-4 e-h-start e-v-center'}>
               <Card className={'e-background-white full-width card-sign'}>
@@ -97,11 +194,11 @@ class PatagoniaForms extends React.Component {
                   <Input type={'email'} name={'label'} label={'Email'} classes={'e-text-grey-300 input-look'}/>
                   <Input type={'text'} name={'label'} label={'Password'} classes={'e-text-grey-300 input-look'}/>
 
-                  <Block className={'e-left remember-display'}>
+                  <Block className={'e-left remember-display e-margin-top-15'}>
                     <Switch type={'checkbox'} text={'Remember me'} name={'switch-uncheckbox'} />
                   </Block>
 
-                  <Block className={'e-right'}>
+                  <Block className={'e-right e-margin-top-15'}>
                     <Text type={'a'} classes={'e-text-right e-body1 e-text-cyan-A200 '}>Forgot password?</Text>
                   </Block>
 
@@ -120,11 +217,11 @@ class PatagoniaForms extends React.Component {
                   <Icon name={"navigation-close"} className={"e-text-black e-right"} />
                 </CardHeader>
                 <CardContent>
-                  <Input type={'email'} name={'label'} label={'Email'} classes={'input-look input-focus'}/>
+                  <Input type={'email'} name={'label'} label={'weerdlook@gmail.com'} classes={'input-look input-focus'}/>
                   <Input type={'text'} name={'label'} label={'Password'} classes={'e-text-grey-300 input-look'}/>
                   <Input type={'text'} name={'label'} label={'Confirm password'} classes={'e-text-grey-300 input-look'}/>
 
-                  <Block className={'e-left'}>
+                  <Block className={'e-left e-margin-top-15'}>
                       <Text type={'a'}>
                         <Icon name={"navigation-apps"} className={"e-text-cyan-A200 e-left"} />
                         <Block classes={'content e-left e-no-padding'}>
@@ -133,7 +230,7 @@ class PatagoniaForms extends React.Component {
                       </Text>
                   </Block>
 
-                  <Block className={'e-right'}>
+                  <Block className={'e-right e-margin-top-15'}>
                     <Switch type={'radio'} classes={'e-left e-text-cyan-A200'} name='switch-radio' text={'Male'}/>
                     <Switch type={'radio'} classes={'e-right e-text-cyan-A200'} name='switch-radio' text={'Female'}/>
                   </Block>
@@ -145,11 +242,11 @@ class PatagoniaForms extends React.Component {
               </Card>
             </Block>
 
-            <Block classes={'brick brick-4 e-h-end e-v-center'}>
+            <Block classes={'brick brick-4 e-h-end e-v-end'}>
               <Card classes={'full-width bg-city e-text-white card-sign'}>
                 <CardHeader className={'e-text-left border-header-card'}>
-                  <Text type={'span'} classes={'e-text-left e-body1 e-text-white '}>Sign In</Text>
-                  <Icon name={"hardware-keyboard-control"} className={"e-text-white e-display-1 e-right"} />
+                  <Text type={'span'} classes={'e-text-left e-body1 e-text-white'}>Sign In</Text>
+                  <Icon name={"hardware-keyboard-control"} className={"e-text-white e-headline e-right"} />
                 </CardHeader>
                 <CardContent>
                   <Text type={'h1'} classes={'e-text-left e-text-white e-headline'}>Kohalo&Co</Text>
@@ -195,9 +292,7 @@ class PatagoniaForms extends React.Component {
                 <CardContent className={'e-text-center'}>
                   <Input type={'email'} name={'label'} label={'Email'} classes={'e-text-grey-300 input-look'}/>
                   <Input type={'text'} name={'label'} label={'Password'} classes={'e-text-grey-300 input-look'}/>
-
                   <Text type={'a'} classes={'e-text-center e-body1 e-text-cyan-A200 '}>Forgot password?</Text>
-
                   <Block>
                     <Btn label={'SIGN IN'} ripple={true} type={'submit'} classes={'flat e-margin-top-25 e-background-cyan-A200 e-text-white full-width'} />
                   </Block>
@@ -206,9 +301,9 @@ class PatagoniaForms extends React.Component {
                   <Block>
                     <Text type={'p'} classes={'e-text-center e-body1 e-text-grey-500 e-margin-top-15 para-inter'}>or</Text>
                   </Block>
-                  <Icon name={"action-account-circle"} className={"e-text-grey-500 e-display-1 e-margin-bottom-15 margin-r-icon"} />
-                  <Icon name={"action-face-unlock"} className={"e-text-grey-500 e-display-1 margin-r-icon"} />
-                  <Icon name={"action-stars"} className={"e-text-grey-500 e-display-1"} />
+                    <Icon name={"action-account-circle"} className={"e-text-grey-500 e-display-1 margin-r-icon"} />
+                    <Icon name={"action-face-unlock"} className={"e-text-grey-500 e-display-1 margin-r-icon"} />
+                    <Icon name={"action-stars"} className={"e-text-grey-500 e-display-1"} />
                 </CardFooter>
               </Card>
             </Block>
@@ -223,6 +318,7 @@ class PatagoniaForms extends React.Component {
                 </CardContent>
               </Card>
             </Block>
+
             <Block classes={'brick brick-4 e-h-end e-v-center'}>
               <Block classes={'e-row full-width'}>
                 <Block classes={'brick brick-12 e-h-center e-v-center'}>
@@ -247,7 +343,7 @@ class PatagoniaForms extends React.Component {
                       <Icon name={"navigation-close"} className={"e-text-black e-right"} />
                     </CardHeader>
                     <CardContent className={'e-text-center'}>
-                      <Input type={'email'} name={'label'} label={'Email'} classes={'e-text-grey-300 input-look input-focus'}/>
+                      <Input type={'email'} name={'label'} label={'weerdlook@gmail.com'} classes={'e-text-grey-300 input-look input-focus'}/>
                       <Block>
                         <Btn label={'Change email'} ripple={true} type={'submit'} classes={'raised e-button e-background-indigo-400 e-text-white e-margin-top-15 full-width'} />                      </Block>
                     </CardContent>
@@ -257,6 +353,7 @@ class PatagoniaForms extends React.Component {
 
             </Block>
           </Block>
+
           <Block classes={'e-row'}>
             <Block classes={'brick brick-4 e-h-start e-v-end'}>
               <Card className={'e-background-white full-width'}>
